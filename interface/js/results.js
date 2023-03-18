228c666de6f6d53c330387feee16187d264316c0
Vitor-C-Souza <vitor_souza97@hotmail.com>
2023-03-17 22:54:07 -0300

    projeto finalizado

--- interface/js/results.js
+++ interface/js/results.js
@@ -1,21 +1,29 @@
+if(!window.localStorage.getItem('token')){
+  window.location.href = '/login.html'
+}
+
 const queryString = new URLSearchParams(window.location.search)
 const resultId = queryString.get('id')
 
 const bail = () => {
   alert('Nenhum resultado encontrado')
-  // window.location = '/'
+  window.location = '/'
 }
 
 if (!resultId) bail()
 
-fetch(`/api/results/${resultId}`)
+fetch(`https://h1ggzduu7a.execute-api.us-east-1.amazonaws.com/api/results/${resultId}`, {
+  headers: {
+    Authorization: `Bearer ${window.localStorage.getItem('token')}`
+  }
+})
   .then((r) => {
     if (!r.ok) bail()
     return r.json()
   })
-  .then(({ result }) => {
+  .then((result) => {
     document.getElementById('student-name').innerText = result.name
-    document.getElementById('correct').innerText = result.correctAnswers
+    document.getElementById('correct').innerText = result.totalCorrectAnswers
   })
   .catch((e) => {
     console.error(e)
